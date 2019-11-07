086829d9ff7fe458f5b8d8d89f84c5bcdb4e5879
Mashkovska <32987039+Mashkovska@users.noreply.github.com>
2019-11-08 00:12:23 +0300

    lab1

---
+++ Assets/Scripts/FoodGeneration.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodGeneration : MonoBehaviour {
+
+	public float XSize = 12.3f;
+	public float ZSize = 12.3f;
+	public GameObject foodPrefab;
+	public Vector3 curPos;
+	public GameObject curFood;
+	void AddNewFood()
+	{
+	RandomPos();
+	curFood = GameObject.Instantiate(foodPrefab,curPos,Quaternion.identity) as GameObject;
+	}
+	void RandomPos()
+	{
+    curPos = new Vector3(Random.Range(XSize*-1,XSize),0.13f,Random.Range(ZSize*-1,ZSize));
+	}
+
+	void Update()
+	{
+		if(!curFood)
+		{
+			AddNewFood();
+		}
+		else
+		{
+			return;
+		}
+	}
+}
